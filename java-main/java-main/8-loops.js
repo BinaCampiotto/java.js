d864a885eeebe9af3f559acfb88b8b3d6db61064
BINA <126881042+BinaCampiotto@users.noreply.github.com>
2023-08-02 09:56:25 -0300

    Add files via upload

--- java-main/java-main/8-loops.js
+++ java-main/java-main/8-loops.js
@@ -8,11 +8,23 @@ const listaDeDestinos = new Array(
 const idadeComprador = 18;
 const estaAcompanhada = true;
 let temPassagemComprada = false;
-const destino = "Rio de Janeiro";
+const destino = "São Paulo";
 
 console.log("\n Destinos possíveis");
 console.log(listaDeDestinos);
 
 const podeComprar = idadeComprador >= 18 || estaAcompanhada 
 == true;
+
+let contador = 0;
+while(contador<3){
+    if (listaDeDestinos[contador] == destino) {
+        console.log("Destino existe");
+    }else{
+        console.log("Destino não existe");
+    }
+    
+    
+    contador += 1;
+}
 
